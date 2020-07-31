e72a8d9142aed8a35eccb51268e7a8b94ade0853
Pavi532 <udara.sync@gmail.com>
2020-07-31 11:05:07 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,31 @@
+if(process.env.NODE_ENV != 'production'){
+    require('dotenv').config({path: __dirname + '/.env'})
+}
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser:true, useUnifiedTopology: true
+});
+
+
+const indexRouter = require('./routes/routes');
+
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to DB'));
+
+app.use('/',indexRouter);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server running....');
+});
